b0917d4312e392599d73eb2c6d8e86be9b2f354c
grezar <grezar.dev@gmail.com>
2019-08-27 20:51:34 +0900

    Load up to sector 18

--- ipl.asm
+++ ipl.asm
@@ -42,7 +42,7 @@ entry:
         MOV     CH, 0
         MOV     DH, 0
         MOV     CL, 2
-
+readloop:
         MOV     SI, 0
 retry:
         MOV     AH, 0x02
@@ -50,7 +50,7 @@ retry:
         MOV     BX, 0
         MOV     DL, 0x00
         INT     0x13
-        JNC     fin
+        JNC     next
         ADD     SI, 1
         CMP     SI, 5
         JAE     error
@@ -58,6 +58,13 @@ retry:
         MOV     DL, 0x00
         INT     0x13
         JMP     retry
+next:
+        MOV     AX, ES
+        ADD     AX, 0x0020
+        MOV     ES, AX
+        ADD     CL, 1
+        CMP     CL, 18
+        JBE     readloop
 putloop:
         MOV     AL, [SI]
         ADD     SI, 1
