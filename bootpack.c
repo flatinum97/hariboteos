bd9332f344565f3603685db2bb09274800ccefc2
supernetnavi <supernetnavi+github@gmail.com>
2019-09-23 11:29:52 +0900

    harib03c

--- bootpack.c
+++ bootpack.c
@@ -1,44 +1,4 @@
-#define COL8_000000  0
-#define COL8_FF0000  1
-#define COL8_00FF00  2
-#define COL8_FFFF00  3
-#define COL8_0000FF  4
-#define COL8_FF00FF  5
-#define COL8_00FFFF  6
-#define COL8_FFFFFF  7
-#define COL8_C6C6C6  8
-#define COL8_840000  9
-#define COL8_008400 10
-#define COL8_848400 11
-#define COL8_000084 12
-#define COL8_840084 13
-#define COL8_008484 14
-#define COL8_848484 15
-
-struct BOOTINFO {
-    char cyls, leds, vmode, reserve;
-    short scrnx, scrny;
-    char *vram;
-};
-
-struct SEGMENT_DESCRIPTOR {
-    short limit_low, base_low;
-    char base_mid, access_right;
-    char limit_high, base_high;
-};
-
-struct GATE_DESCRIPTOR {
-    short offset_low, selector;
-    char dw_count, access_right;
-    short offset_high;
-};
-
-void io_hlt(void);
-
-void init_palette(void);
-void init_screen(char *vram, int xsize, int ysize);
-void init_mouse_cursor8(char *mouse, char bc);
-void init_gdtidt(void);
+#include "bootpack.h"
 
 void HariMain(void)
 {
--- dsctbl.c
+++ dsctbl.c
@@ -1,3 +1,5 @@
+#include "bootpack.h"
+
 #define COL8_000000  0
 #define COL8_FF0000  1
 #define COL8_00FF00  2
@@ -14,44 +16,6 @@
 #define COL8_840084 13
 #define COL8_008484 14
 #define COL8_848484 15
-
-struct BOOTINFO {
-    char cyls, leds, vmode, reserve;
-    short scrnx, scrny;
-    char *vram;
-};
-
-struct SEGMENT_DESCRIPTOR {
-    short limit_low, base_low;
-    char base_mid, access_right;
-    char limit_high, base_high;
-};
-
-struct GATE_DESCRIPTOR {
-    short offset_low, selector;
-    char dw_count, access_right;
-    short offset_high;
-};
-
-void io_hlt(void);
-void io_cli(void);
-void io_out8(int port, int data);
-int io_load_eflags(void);
-void io_store_eflags(int eflags);
-void load_gdtr(int limit, int addr);
-void load_idtr(int limit, int addr);
-
-void init_palette(void);
-void init_screen(char *vram, int xsize, int ysize);
-void init_mouse_cursor8(char *mouse, char bc);
-void init_gdtidt(void);
-void set_segmdesc(struct SEGMENT_DESCRIPTOR *sd, unsigned int limit, int base, int ar);
-void set_gatedesc(struct GATE_DESCRIPTOR *gd, int offset, int selector, int ar);
-void set_palette(int start, int end, unsigned char *rgb);
-void boxfill8(unsigned char *vram, int xsize, unsigned char c, int x0, int y0, int x1, int y1);
-void putfont8(char *vram, int xsize, int x, int y, char c, char *font);
-void putfont8_asc(char *vram, int xsize, int x, int y, char c, unsigned char *s);
-void putblock_8(char *vram, int vxsize, int pxsize, int pysize, int px0, int py0, char *buf, int bxsize);
 
 void init_gdtidt(void) {
     struct SEGMENT_DESCRIPTOR *gdt = (struct SEGMENT_DESCRIPTOR *) 0x00270000;
